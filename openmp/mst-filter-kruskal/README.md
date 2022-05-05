6c53daa4c9863b1ec09d7608b465c081f2bd4578
chunhp <tezktenr@gmail.com>
2022-05-04 17:44:29 -0700

    refactored mst-kruskal-samplesort solution

---
+++ openmp/mst-filter-kruskal/README.md
@@ -0,0 +1,59 @@
+# OpenMP Implementation (Kruskal with Parallel Sample Sort)
+This is the OpenMP implementation (Kruskal with Parallel Sample Sort) for finding the set of nearest balanced states of a signed graph
+
+## Building Our Code
+First, we need to make sure that the CMake module is loaded and that the GNU compiler is selected.
+```
+student@cori04:~/gradproj> module load cmake
+student@cori04:~/gradproj> module swap PrgEnv-intel PrgEnv-gnu
+```
+You should put these commands in your ~/.bash_profile file to avoid typing them every time you log in.
+
+Next, let's build the code. CMake prefers out of tree builds, so we start by creating a build directory.
+
+```
+student@cori04:~/gradproj> mkdir build
+student@cori04:~/gradproj> cd build
+student@cori04:~/gradproj/build>
+```
+
+Next, we have to configure our build. We can either build our code in Debug mode or Release mode. In debug mode, optimizations are disabled and debug symbols are embedded in the binary for easier debugging with GDB. In release mode, optimizations are enabled, and debug symbols are omitted. For example:
+```
+student@cori04:~/xxx/build> cmake -DCMAKE_BUILD_TYPE=Release ..
+-- The C compiler identification is GNU 8.3.0
+...
+-- Configuring done
+-- Generating done
+-- Build files have been written to: /global/homes/s/students/gradproj/build
+```
+
+Once our build is configured, we may actually execute the build:
+```
+student@cori04:~/gradproj/build> make
+```
+
+Then, we could run the code via:
+```
+student@cori04:~/gradproj/build> sbatch job-openmp
+```
+
+## Profiling
+You should uncomment the the following macro in "main.cpp" and **recompile** to perform profiling:
+```C++
+#define PROFILING
+```
+
+## Checking Correctness
+You **must uncomment** the following marcro in "main.cpp" and **recompile** to perform correctness check:
+```C++
+#define TEST_CORRECTNESS
+```
+Generating the output files is similar to the serial implementation. To run with 1000 iterations and seed 1, simply add the following line to "job-openmp":
+```
+srun -n 1 -c 68 --cpu_bind=cores ./openmp -f ../../../sample-graphs/graph2.csv -n 1000 -s 1 -o openmp.out
+```
+Then, run with
+```
+student@cori04:~/gradproj/build> sbatch job-openmp
+```
+
---
+++ openmp/mst-filter-kruskal/common.hpp
@@ -0,0 +1,130 @@
+#ifndef __COMMON_HPP__
+#define __COMMON_HPP__
+
+/********
+ * INCLUDE HEADER
+ ***/
+#include <iostream>
+#include <chrono>
+
+#include "mst.hpp"
+
+
+/********
+ * PRE-PROCESSOR
+ ***/
+
+
+/********
+ * COMMON DATA STRUCTURE
+ ***/
+struct profile_times {
+	std::chrono::time_point<std::chrono::steady_clock> gen_random_start;
+	std::chrono::time_point<std::chrono::steady_clock> gen_random_end;
+	std::chrono::time_point<std::chrono::steady_clock> find_mst_start;
+	std::chrono::time_point<std::chrono::steady_clock> find_mst_end;
+	std::chrono::time_point<std::chrono::steady_clock> clear_start;
+	std::chrono::time_point<std::chrono::steady_clock> clear_end;
+	std::chrono::time_point<std::chrono::steady_clock> sort_start;
+	std::chrono::time_point<std::chrono::steady_clock> sort_end;
+	std::chrono::time_point<std::chrono::steady_clock> pq_start;
+	std::chrono::time_point<std::chrono::steady_clock> pq_end;
+	std::chrono::time_point<std::chrono::steady_clock> compress_start;
+	std::chrono::time_point<std::chrono::steady_clock> compress_end;
+	std::chrono::time_point<std::chrono::steady_clock> gen_tree_start;
+	std::chrono::time_point<std::chrono::steady_clock> gen_tree_end;
+	std::chrono::time_point<std::chrono::steady_clock> balance_start;
+	std::chrono::time_point<std::chrono::steady_clock> balance_end;
+	std::chrono::time_point<std::chrono::steady_clock> start_time;
+	std::chrono::time_point<std::chrono::steady_clock> end_time;
+
+	double time(std::chrono::time_point<std::chrono::steady_clock> start,
+				std::chrono::time_point<std::chrono::steady_clock> end)
+	{
+		return ((std::chrono::duration<double>)(end - start)).count();
+	}
+};
+
+
+struct algorithm_params {
+	// total number of nodes
+	int V;
+
+	// total number of edges
+	int E;	
+
+	// iterations
+	int iterations;
+
+	// the seed
+	unsigned int seed;
+};
+
+
+struct common_data {
+	// the updated edge weights (1 or -1) after balancing for an iteration
+	int* balancedWeights;
+
+
+	// -----------------------------
+	// destructor for memory release
+	// -----------------------------
+	~common_data() 
+	{
+		if (balancedWeights != nullptr)
+			delete[] balancedWeights;
+	}
+};
+
+
+struct mst_data {
+	// indicate whether an edge is selected to be in the MST for an iteration
+	bool* inMST;
+
+	// buffer for edges random weights used in MST
+	edge_w* edge_mst_buf;
+
+	// sorted edges random weights used in MST (if NOT sorted in place)
+	edge_w* edge_mst_queue;
+	
+
+	// -----------------------------
+	// destructor for memory release
+	// -----------------------------
+	~mst_data() 
+	{
+		if (inMST != nullptr)
+			delete[] inMST;
+		if (edge_mst_buf != nullptr)
+			delete[] edge_mst_buf;
+		if (edge_mst_queue != nullptr)
+			delete[] edge_mst_queue;
+	}
+};
+
+
+struct sample_sort_data {
+	// number of threads intended to be used for sorting
+	int thread_nums;
+
+	// sample edges list used during sample sort
+	edge_w* sample_edges;
+
+	// the start index for each bucket of sample sort
+	int* bucket_starts;
+	
+
+	// -----------------------------
+	// destructor for memory release
+	// -----------------------------
+	~sample_sort_data() 
+	{
+		if (sample_edges != nullptr)
+			delete[] sample_edges;
+		if (bucket_starts != nullptr)
+			delete[] bucket_starts;
+	}
+};
+
+
+#endif
